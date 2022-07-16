2af68e5ca623b119c6e490b973e9aa045741b8a6
Tom Do <tom@iifuture.com>
2022-07-16 17:45:34 +1000

    3 done

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,47 @@ fn q2(ceilnum : i32) -> i32 {
     return evensum;
 }
 
+fn nthprime(n : i64) -> i64 {
+    let mut prevprimes = vec![2];
+    let mut iter : i64 = 1;
+    let mut num : i64 = 2;
+    while iter < n {
+        num += 1;
+        if prevprimes.iter().all(|x| num % x != 0) {
+            prevprimes.push(num);
+            iter += 1;
+        }
+    }
+    return num;
+}
+
+fn q3(mut primenum : i64) -> i64 {
+    let mut n : i64 = 1;
+    let mut divisor : i64 = nthprime(n);
+    while primenum >= divisor {
+        n += 1;
+        divisor = nthprime(n);
+        if primenum % divisor == 0 {
+            primenum /= divisor;
+        }
+    }
+    return divisor;
+}
+
+fn q4() -> i32 {
+    let mut num = 0;
+    for x in 1..1000 {
+        for y in 1..1000 {
+            num = x * y;
+        }
+    }
+    let strnum : String = format!("{:?}", num);
+    return 12
+}
+
 fn main() {
-    println!("{}",q1(1000));
-    println!("{}",q2(1000));
+    //println!("{}",q1(1000));
+    //println!("{}",q2(4000000));
+    //println!("{}",q3(600851475143));
+    println!("{}",q4());
 }
