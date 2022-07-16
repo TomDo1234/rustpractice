c8085ccc07e7a6c8a308923919d7733396309654
Tom Do <tom@iifuture.com>
2022-07-16 17:14:46 +1000

    Q1 and 2 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+
+fn q1(ceilnum : i32) -> i32 {
+    let mut sum: i32 = 0;
+    for i in 1..ceilnum {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i
+        }
+    }
+    return sum;
+}
+
+fn q2(ceilnum : i32) -> i32 {
+    let mut sumbefore : i32 = 1;
+    let mut sum: i32 = 1;
+    let mut evensum : i32 = 0;
+    while sum <= ceilnum {
+        let oldsumbefore = sumbefore;
+        sumbefore = sum;
+        sum += oldsumbefore;
+        if sum % 2 == 0 {
+            evensum += sum;
+        }
+    }
+    return evensum;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}",q1(1000));
+    println!("{}",q2(1000));
 }
