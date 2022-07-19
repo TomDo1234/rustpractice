4e9ef5418b7f00af55b3e0119c6d45cbc24225d8
Tom Do <tom@iifuture.com>
2022-07-19 12:28:15 +1000

    20 done

--- src/main.rs
+++ src/main.rs
@@ -323,31 +323,14 @@ fn q17(num : i32) -> usize {
     return string.chars().count();
 }
 
-fn q18() -> i32 {
-    let tree : Vec<Vec<i32>> = vec![
-        vec![75],
-        vec![95,64],
-        vec![17,47,82],
-        vec![18,35,87,10],
-        vec![20,04,82,47,65],
-        vec![19,01,23,75,03,34],
-        vec![88,02,77,73,07,63,67],
-        vec![99,65,04,28,06,16,70,92],
-        vec![41,41,26,56,83,40,80,70,33],
-        vec![41,48,72,33,47,32,37,16,94,29],
-        vec![53,71,44,65,25,43,91,52,97,51,14],
-        vec![70,11,33,28,77,73,17,78,39,68,17,57],
-        vec![91,71,52,38,17,14,91,43,58,50,27,29,48],
-        vec![63,66,04,68,89,53,67,30,73,16,69,87,40,31],
-        vec![04,62,98,27,23,09,70,98,73,93,38,53,60,04,23]
-    ];    
+fn q18(tree : Vec<Vec<i32>>) -> i32 { 
     fn recursion(tree : &Vec<Vec<i32>>,branchindex : usize , level : usize) -> Vec<i32> {
         let mut results = vec![];
         for i in branchindex..branchindex+2 {
-            if level == 14 {
+            if level == tree.len() - 1 {
                 results.push(tree[level][i])
             }
-            else if level < 14  {
+            else if level < tree.len() - 1  {
                 let next = recursion(tree,i,level + 1);
                 let mut result = tree[level][i];
                 if next[0] > next[1] {
@@ -365,16 +348,48 @@ fn q18() -> i32 {
         }
         return results;
     }   
-    let topnum = tree[0][0];
-    return *recursion(&tree,0,1).iter().max().unwrap() + topnum;
+    return *recursion(&tree,0,1).iter().max().unwrap() + &tree[0][0];
+}
+
+fn q19() -> i32 {
+    let normalyearfirstdays = vec![1,32,60,91,121,152,182,213,243,274,304,335];
+    let leapyearfirstdays = vec![1,32,61,92,122,153,183,214,244,275,305,336];
+    let mut yearcounter = 1900;
+    let mut daycounter = 1;
+    let mut sundaycounter = 0;
+    for i in 0..36500 {
+        let yearlength = if yearcounter % 4 == 0 { 366 } else { 365 };
+        let yearfirstdays = if yearcounter % 4 == 0 { &leapyearfirstdays } else { &normalyearfirstdays };
+        if i % 7 == 0 && yearfirstdays.contains(&daycounter) {
+            sundaycounter += 1;
+        }
+        if daycounter >= yearlength {
+            daycounter = 0;
+            yearcounter += 1;
+        }
+        daycounter += 1;
+    }
+    return sundaycounter;
+}
+
+fn q20(upper : i32) -> u32 {
+    let mut init = BigInt::from(1 as i32);
+    for i in 2..upper + 1 {
+        init = init.checked_mul(&BigInt::from(i)).unwrap();
+    }
+    let string = init.to_str_radix(10);
+    return string.chars().map(|ch| ch.to_digit(10).unwrap()).sum();
+}
+
+
+
+fn q21() -> Vec<i32> {
+    let a = divisors(50).pop().iter().sum(); // -1 because 1 is a divisor
+    let b = divisors(70).pop().iter().sum();
+    return vec![a,b];
 }
 
 fn main() {
-    //println!("{}",q14(1000000));
-    //println!("{}",q15());
-    //println!("{}",q16());
-    //println!("{}",q17(1000));
-    println!("{}",q18());
-   
-
+    //println!("{}",q19());
+    println!("{:?}",q21());
 }
